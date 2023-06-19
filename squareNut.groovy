bc159fb4778915d1a472cfa819f029754c93f07f
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-19 13:40:48 -0400

    aquare nut

--- squareNut.groovy
+++ squareNut.groovy
@@ -1,4 +1,5 @@
 import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
 import eu.mihosoft.vrl.v3d.Cylinder
 import eu.mihosoft.vrl.v3d.parametrics.*;
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
@@ -23,7 +24,7 @@ import com.google.gson.reflect.TypeToken;
 	
 import eu.mihosoft.vrl.v3d.parametrics.*;
 CSG getNut(){
-	String type= "capScrew"
+	String type= "squareNut"
 	LengthParameter facets		= new LengthParameter("Bolt Hole Facet Count",10,[40,10])
 	LengthParameter boltLength		= new LengthParameter("Bolt Length",10,[180,10])
 	LengthParameter offset		= new LengthParameter("printerOffset",0.0,[2,0])
@@ -35,21 +36,14 @@ CSG getNut(){
 										Vitamins.listVitaminSizes(type))
 	//println "Database loaded "+database
 	HashMap<String,Object> servoConfig = Vitamins.getConfiguration( type,size.getStrValue())
-	headDiameter=Double.parseDouble(servoConfig.get("headDiameter").toString())+offset.getMM()
-	headHeight=Double.parseDouble(servoConfig.get("headHeight").toString())
-	keyDepth=Double.parseDouble(servoConfig.get("keyDepth").toString())
-	keySize=Double.parseDouble(servoConfig.get("keySize").toString())
-	outerDiameter=Double.parseDouble(servoConfig.get("outerDiameter").toString())+offset.getMM()
-	if(servoConfig.get("boltLength")!=null)
-		boltLength.setMM(servoConfig.get("boltLength"))
+	double w=Double.parseDouble(servoConfig.get("width").toString())+offset.getMM()
+	double h=Double.parseDouble(servoConfig.get("height").toString())
 
 	
-	CSG head =new Cylinder(headDiameter/2,headDiameter/2,headHeight,(int)facets.getMM()).toCSG() // a one line Cylinder
+	CSG head =new Cube(w,w,h).toCSG() // a one line Cylinder
 				.toZMin()
-	CSG shaft =new Cylinder(outerDiameter/2,outerDiameter/2,boltLength.getMM(),(int)facets.getMM()).toCSG() // a one line Cylinder
-				.toZMax()
-	
-	return head.union(shaft)
+
+	return head
 		.setParameter(size)
 		.setParameter(boltLength)
 		.setParameter(facets)
